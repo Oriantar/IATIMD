076b318235ae349412a4930ea2d978a0e55fa503
Oriantar <tim.maurizio@gmail.com>
2024-05-07 16:25:37 +0200

    added weather support

--- IATIMD/App.js
+++ IATIMD/App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import Geolocation from 'react-native-geolocation-service';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const apiKey = "5b9ab3da472582e0607641ff35664d31";
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=" + apiKey;
+  const [weather, setData] = useState([]);
+  const [temp, setTemp] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios.get(apiUrl)
+      .then((response) => {
+        setData(response.data.weather[0].description);
+        setTemp(response.data.main.temp);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+      });
 
+  }, []);
+
+
+      
+  if(!loading){
+    return (
+      
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text>Dit is de header</Text>
+        </View>
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}> . {weather}</Text>
+        <Text style={styles.text}> . {temp}</Text>
+        <StatusBar style="auto" />
+      </View>
+
+    );}
+  
+  
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    flexDirection: ' column',
   },
+  text: {
+    color: '#fff',
+  },
+  header: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    height: 100,
+  },
+  footer: {
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 0.2,
+  }
 });
